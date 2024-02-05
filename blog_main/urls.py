f239f922a4102b265de23f8fe5c8d19190fadcf4
Abdul Syed <138042476+AbdulSyed05@users.noreply.github.com>
2024-02-05 19:29:24 +0000

    blogs and search features

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,9 +19,14 @@ from django.urls import include, path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from blogs import views as BlogsView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-      path('category/', include('blogs.urls')),
+    path('category/', include('blogs.urls')),
+    path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
+    # Search endpoint
+    path('search/', BlogsView.search, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
