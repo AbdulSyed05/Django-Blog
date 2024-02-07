73aa6e367aab53039a762c1ffaf0b4da4a503626
Abdul Syed <138042476+AbdulSyed05@users.noreply.github.com>
2024-02-07 11:16:36 +0000

    Post and Crud operations added and created

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from blogs import views as BlogsView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -32,7 +31,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    
+
     # Dashboards
     path('dashboard/', include('dashboards.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from blogs.models import Category
+from blogs.models import Blog, Category
 
 
 
@@ -9,3 +9,8 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured')
