00903693a34e8555e7d92e9037c33fa10fba8b00
Abdul Syed <138042476+AbdulSyed05@users.noreply.github.com>
2024-02-21 12:28:42 +0000

    changed allowed host settings

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-n8^@1$ll9wyo)$h5h+5xq-a7a!vnn&@5(gi#!3*9#t@=nqc&al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangobblog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['djangobblog-3840dbf28a3e.herokuapp.com']
 
 
 # Application definition
