496472215d8645f4f0e52f0e4bec4d73e5c99152
Abdul Syed <138042476+AbdulSyed05@users.noreply.github.com>
2024-02-21 12:21:30 +0000

    set debug to True

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n8^@1$ll9wyo)$h5h+5xq-a7a!vnn&@5(gi#!3*9#t@=nqc&al'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['djangobblog.herokuapp.com', 'localhost']
 
