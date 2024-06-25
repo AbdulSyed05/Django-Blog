274d334e0b37c9756236273258a0a434605c2b55
Abdul Syed <abdul.ssyed@live.co.uk>
2024-06-25 20:41:23 +0000

    defaultstorage changed

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 ALLOWED_HOSTS = [
     "djangobblog-4328bed47b45.herokuapp.com",
@@ -170,7 +172,7 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # MEDIA_URL = '/media/'-
 # MEDIA_ROOT = BASE_DIR /'media'
@@ -178,4 +180,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
-# DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
