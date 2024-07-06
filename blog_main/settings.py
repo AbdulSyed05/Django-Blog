b65918cabb5cc8da2c390571a1731974fc3fd6f9
Abdul Muqeet Syed <abdul.ssyed@live.co.uk>
2024-07-06 12:58:03 +0000

    removed additional storage

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -163,9 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
@@ -180,5 +180,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # MEDIA_ROOT = BASE_DIR /'media'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
